c5d47d77952c6de2479f40dece93b664aa438e8e
dnoseda <damian.noseda@mercadolibre.com>
2010-10-22 16:44:04 -0300

    nueva versión

--- UiPerformanceGrailsPlugin.groovy
+++ UiPerformanceGrailsPlugin.groovy
@@ -11,7 +11,7 @@ import com.studentsonly.grails.plugins.uiperformance.postprocess.JsTagPostProces
 
 class UiPerformanceGrailsPlugin {
 
-	String version = '1.2.3'
+	String version = '1.2.4'
 	String grailsVersion = '1.0 > *'
 	Map dependsOn = [:]
 	List pluginExcludes = [
